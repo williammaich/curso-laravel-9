3d41c872ddcb8ce7d6704f1a03b1d1bc50008f38
williammaich <williammaich@hotmail.com>
2022-05-04 13:55:35 -0300

    com erro não esta salvando no banco;

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index(){
+
+        $users = User::get();
+        dd($users);
         return view('users.index');
     }
 
---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use \App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        User::create([
+            'name' => 'Carlos Ferreira',
+            'email' => 'carlos@especializati.com.br',
+            'password' => bcsqrt('12345678'),
+        ]);*/
+    }
+}
